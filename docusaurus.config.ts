5c9d8480948704c2697cdf13a33840fd49930532
Pebloop <pebloop@outlook.com>
2024-11-18 16:37:41 +0100

    Fix baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://wiki.wordly-builder.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wordly-wiki/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
